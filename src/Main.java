190140ab7596e16d6c6f03a909f2f2e8d1dab1a3
EduardoF10 <eduardo.fuertes1@upr.edu>
2023-08-22 19:40:59 -0400

    Initial Commit with newly implemented feature that waits for captcha completion when prompted

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
+import com.gpt.ChatBot;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
-
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
-
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://formy-project.herokuapp.com/form");
-
-        driver.quit();
-
+    public static void main(String[] args) throws InterruptedException {
+        ChatBot chatTest = new ChatBot();
+        chatTest.promptTester();
     }
 }
